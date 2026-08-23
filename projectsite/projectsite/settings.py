a967717d0b88a47aa075847a56583bfd087f4d93
Ch8967 <davedenzon76@gmail.com>
2026-08-23 22:01:11 +0800

    Updated ALLOWED_HOSTS in settings.py

--- projectsite/projectsite/settings.py
+++ projectsite/projectsite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-s%kp-jhf^_9h2r7ntb*@%c5z2wds*_5d6k1f&dnrgn+djye(2o
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['psusphere.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
